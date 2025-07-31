bfd352261263a545d5eb96047351561f3e6f0ea1
Jason Sadler <jason@sadlerjw.com>
2025-07-31 19:41:26 -0400

    Add ROR instruction

---
+++ NES-iOS/Emulator/Instructions/ROR.swift
@@ -0,0 +1,51 @@
+//
+//  ASL.swift
+//  NES-iOS
+//
+//  Created by Jason Sadler on 2025-07-30.
+//
+
+extension Instructions {
+    struct ROR : Instruction {
+        static let sharedInstance = Self.init()
+        
+        static let opcodeReferences : [OpcodeReference] = [
+            .init(opcode: 0x6A,
+                  totalBytes: 1,
+                  defaultCycles: 2,
+                  instruction: Self.sharedInstance,
+                  addressingMode: AddressingModes.Accumulator.sharedInstance),
+            .init(opcode: 0x66,
+                  totalBytes: 2,
+                  defaultCycles: 5,
+                  instruction: Self.sharedInstance,
+                  addressingMode: AddressingModes.ZeroPage.sharedInstance),
+            .init(opcode: 0x76,
+                  totalBytes: 2,
+                  defaultCycles: 6,
+                  instruction: Self.sharedInstance,
+                  addressingMode: AddressingModes.ZeroPageX.sharedInstance),
+            .init(opcode: 0x6E,
+                  totalBytes: 3,
+                  defaultCycles: 6,
+                  instruction: Self.sharedInstance,
+                  addressingMode: AddressingModes.Absolute.sharedInstance),
+            .init(opcode: 0x7E,
+                  totalBytes: 3,
+                  defaultCycles: 7,
+                  instruction: Self.sharedInstance,
+                  addressingMode: AddressingModes.AbsoluteX.sharedInstance),
+            
+        ]
+        
+        func execute(cpu: borrowing CPU) -> ReadModifyWriteResult? {
+            let result = cpu.fetchedData >> 1 | ((cpu.fetchedData & 0x01) << 7)
+            
+            cpu.status.setC(cpu.fetchedData & 0x01 != 0)
+            cpu.status.setZ(result == 0)
+            cpu.status.setN(result & 0x80 != 0)
+            
+            return result
+        }
+    }
+}
---
+++ NES-iOSTests/InstructionTests/RORTests.swift
@@ -0,0 +1,74 @@
+//
+//  ASLTests.swift
+//  NES-iOSTests
+//
+//  Created by Jason Sadler on 2025-07-30.
+//
+
+import Testing
+@testable import NES_iOS
+
+@MainActor struct RORTests {
+    let nes = NES()
+    let ror = Instructions.ROR()
+    var cpu: CPU {
+        return nes.cpu
+    }
+    
+    @Test func testNormal() async throws {
+        cpu.status.insert(.c)
+        cpu.status.insert(.z)
+        cpu.status.insert(.n)
+        cpu.fetchedData = 0b00010010
+        let expected =    0b00001001
+        
+        let result = ror.execute(cpu: cpu)
+        
+        if let result {
+            #expect(result == expected)
+        } else {
+            #expect(Bool(false), "Should not return nil")
+        }
+        #expect(!cpu.status.contains(.c))
+        #expect(!cpu.status.contains(.z))
+        #expect(!cpu.status.contains(.n))
+    }
+    
+    @Test func testCarryNegative() async throws {
+        cpu.status.remove(.c)
+        cpu.status.insert(.z)
+        cpu.status.remove(.n)
+        cpu.fetchedData = 0b11011011
+        let expected =    0b11101101
+        
+        let result = ror.execute(cpu: cpu)
+        
+        if let result {
+            #expect(result == expected)
+        } else {
+            #expect(Bool(false), "Should not return nil")
+        }
+        #expect(cpu.status.contains(.c))
+        #expect(!cpu.status.contains(.z))
+        #expect(cpu.status.contains(.n))
+    }
+    
+    @Test func testZero() async throws {
+        cpu.status.remove(.c)
+        cpu.status.remove(.z)
+        cpu.status.insert(.n)
+        cpu.fetchedData = 0b00000000
+        let expected =    0b00000000
+        
+        let result = ror.execute(cpu: cpu)
+        
+        if let result {
+            #expect(result == expected)
+        } else {
+            #expect(Bool(false), "Should not return nil")
+        }
+        #expect(!cpu.status.contains(.c))
+        #expect(cpu.status.contains(.z))
+        #expect(!cpu.status.contains(.n))
+    }
+}
