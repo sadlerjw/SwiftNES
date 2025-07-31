07303366bfdfbf5aab72af44a809057f06a1617b
Jason Sadler <jason@sadlerjw.com>
2025-07-31 18:08:45 -0400

    Add LSR instruction

--- NES-iOS/Emulator/Instructions/LSR.swift
+++ NES-iOS/Emulator/Instructions/LSR.swift
@@ -6,31 +6,31 @@
 //
 
 extension Instructions {
-    struct ASL : Instruction {
+    struct LSR : Instruction {
         static let sharedInstance = Self.init()
         
         static let opcodeReferences : [OpcodeReference] = [
-            .init(opcode: 0x0A,
+            .init(opcode: 0x4A,
                   totalBytes: 1,
                   defaultCycles: 2,
                   instruction: Self.sharedInstance,
                   addressingMode: AddressingModes.Accumulator.sharedInstance),
-            .init(opcode: 0x06,
+            .init(opcode: 0x46,
                   totalBytes: 2,
                   defaultCycles: 5,
                   instruction: Self.sharedInstance,
                   addressingMode: AddressingModes.ZeroPage.sharedInstance),
-            .init(opcode: 0x16,
+            .init(opcode: 0x56,
                   totalBytes: 2,
                   defaultCycles: 6,
                   instruction: Self.sharedInstance,
                   addressingMode: AddressingModes.ZeroPageX.sharedInstance),
-            .init(opcode: 0x0E,
+            .init(opcode: 0x4E,
                   totalBytes: 3,
                   defaultCycles: 6,
                   instruction: Self.sharedInstance,
                   addressingMode: AddressingModes.Absolute.sharedInstance),
-            .init(opcode: 0x1E,
+            .init(opcode: 0x5E,
                   totalBytes: 3,
                   defaultCycles: 7,
                   instruction: Self.sharedInstance,
@@ -39,11 +39,11 @@ extension Instructions {
         ]
         
         func execute(cpu: borrowing CPU) -> ReadModifyWriteResult? {
-            let result = cpu.fetchedData << 1
+            let result = cpu.fetchedData >> 1
             
-            cpu.status.setC(cpu.fetchedData & 0x80 != 0)
+            cpu.status.setC(cpu.fetchedData & 0x01 != 0)
             cpu.status.setZ(result == 0)
-            cpu.status.setN(result & 0x80 != 0)
+            cpu.status.setN(false)
             
             return result
         }
---
+++ NES-iOSTests/InstructionTests/LSRTests.swift
@@ -0,0 +1,74 @@
+//
+//  ASLTests.swift
+//  NES-iOSTests
+//
+//  Created by Jason Sadler on 2025-07-30.
+//
+
+import Testing
+@testable import NES_iOS
+
+@MainActor struct LSRTests {
+    let nes = NES()
+    let lsr = Instructions.LSR()
+    var cpu: CPU {
+        return nes.cpu
+    }
+    
+    @Test func testNormal() async throws {
+        cpu.status.insert(.c)
+        cpu.status.insert(.z)
+        cpu.status.insert(.n)
+        cpu.fetchedData = 0b10010010
+        let expected =    0b01001001
+        
+        let result = lsr.execute(cpu: cpu)
+        
+        if let result {
+            #expect(result == expected)
+        } else {
+            #expect(Bool(false), "Should not return nil")
+        }
+        #expect(!cpu.status.contains(.c))
+        #expect(!cpu.status.contains(.z))
+        #expect(!cpu.status.contains(.n))
+    }
+    
+    @Test func testZeroFromNonZero() async throws {
+        cpu.status.remove(.c)
+        cpu.status.remove(.z)
+        cpu.status.insert(.n)
+        cpu.fetchedData = 0b00000001
+        let expected =    0b00000000
+        
+        let result = lsr.execute(cpu: cpu)
+        
+        if let result {
+            #expect(result == expected)
+        } else {
+            #expect(Bool(false), "Should not return nil")
+        }
+        #expect(cpu.status.contains(.c))
+        #expect(cpu.status.contains(.z))
+        #expect(!cpu.status.contains(.n))
+    }
+    
+    @Test func testZeroFromZero() async throws {
+        cpu.status.insert(.c)
+        cpu.status.remove(.z)
+        cpu.status.insert(.n)
+        cpu.fetchedData = 0b00000000
+        let expected =    0b00000000
+        
+        let result = lsr.execute(cpu: cpu)
+        
+        if let result {
+            #expect(result == expected)
+        } else {
+            #expect(Bool(false), "Should not return nil")
+        }
+        #expect(!cpu.status.contains(.c))
+        #expect(cpu.status.contains(.z))
+        #expect(!cpu.status.contains(.n))
+    }
+}
