b293a9de0d9e515c7f61e48242be8703faec24f3
Jason Sadler <jason@sadlerjw.com>
2025-07-31 17:12:24 -0400

    Add INC instruction

---
+++ NES-iOS/Emulator/Instructions/INC.swift
@@ -0,0 +1,44 @@
+//
+//  DEX.swift
+//  SwiftNES
+//
+//  Created by Jason Sadler on 2025-07-30.
+//
+
+extension Instructions {
+    struct INC : Instruction {
+        static let sharedInstance = Self.init()
+        
+        static let opcodeReferences : [OpcodeReference] = [
+            .init(opcode: 0xE6,
+                  totalBytes: 2,
+                  defaultCycles: 5,
+                  instruction: Self.sharedInstance,
+                  addressingMode: AddressingModes.ZeroPage.sharedInstance),
+            .init(opcode: 0xF6,
+                  totalBytes: 2,
+                  defaultCycles: 6,
+                  instruction: Self.sharedInstance,
+                  addressingMode: AddressingModes.ZeroPageX.sharedInstance),
+            .init(opcode: 0xEE,
+                  totalBytes: 3,
+                  defaultCycles: 6,
+                  instruction: Self.sharedInstance,
+                  addressingMode: AddressingModes.Absolute.sharedInstance),
+            .init(opcode: 0xFE,
+                  totalBytes: 3,
+                  defaultCycles: 7,
+                  instruction: Self.sharedInstance,
+                  addressingMode: AddressingModes.AbsoluteX.sharedInstance),
+        ]
+        
+        @discardableResult
+        func execute(cpu: borrowing CPU) -> ReadModifyWriteResult? {
+            let result = cpu.fetchedData &+ 1
+            cpu.status.setZ(result == 0)
+            cpu.status.setN(result >> 7 == 1)
+            
+            return result
+        }
+    }
+}
---
+++ NES-iOSTests/InstructionTests/INCTests.swift
@@ -0,0 +1,65 @@
+//
+//  DEXTests.swift
+//  NES-iOSTests
+//
+//  Created by Jason Sadler on 2025-07-30.
+//
+
+import Testing
+@testable import NES_iOS
+
+@MainActor struct INCTests {
+    let nes = NES()
+    let inc = Instructions.INC()
+    var cpu: CPU {
+        return nes.cpu
+    }
+
+    @Test func testNormal() async throws {
+        cpu.status.insert(.z)
+        cpu.status.insert(.n)
+        cpu.fetchedData = 0x1A
+        
+        let result = inc.execute(cpu: cpu)
+        
+        #expect(result == 0x1B)
+        #expect(!cpu.status.contains(.z))
+        #expect(!cpu.status.contains(.n))
+    }
+    
+    @Test func testZero() async throws {
+        cpu.status.remove(.z)
+        cpu.status.insert(.n)
+        cpu.fetchedData = 0xFF
+        
+        let result = inc.execute(cpu: cpu)
+        
+        #expect(result == 0x00)
+        #expect(cpu.status.contains(.z))
+        #expect(!cpu.status.contains(.n))
+    }
+
+    @Test func testNegativeFromPositive() async throws {
+        cpu.status.insert(.z)
+        cpu.status.remove(.n)
+        cpu.fetchedData = 0x7F
+        
+        let result = inc.execute(cpu: cpu)
+        
+        #expect(result == 0x80)
+        #expect(!cpu.status.contains(.z))
+        #expect(cpu.status.contains(.n))
+    }
+    
+    @Test func testNegativeFromNegative() async throws {
+        cpu.status.insert(.z)
+        cpu.status.remove(.n)
+        cpu.fetchedData = 0xF8
+        
+        let result = inc.execute(cpu: cpu)
+        
+        #expect(result == 0xF9)
+        #expect(!cpu.status.contains(.z))
+        #expect(cpu.status.contains(.n))
+    }
+}
