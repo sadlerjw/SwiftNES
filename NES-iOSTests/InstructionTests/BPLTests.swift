6e890e8f825b73fea7d8b2f98907e78258e3d321
Jason Sadler <jason@sadlerjw.com>
2025-07-31 00:06:09 -0400

    Add the BPL instruction

---
+++ NES-iOSTests/InstructionTests/BPLTests.swift
@@ -0,0 +1,26 @@
+//
+//  BNETests.swift
+//  NES-iOSTests
+//
+//  Created by Jason Sadler on 2025-07-30.
+//
+
+import Testing
+@testable import NES_iOS
+
+@MainActor struct BPLTests {
+    let nes = NES()
+    let bpl = Instructions.BPL()
+    var cpu: CPU {
+        return nes.cpu
+    }
+    @Test func testTaken() async throws {
+        cpu.status.remove(.n)
+        #expect(bpl.willBranch(cpu: cpu))
+    }
+    
+    @Test func testNotTaken() async throws {
+        cpu.status.insert(.n)
+        #expect(!bpl.willBranch(cpu: cpu))
+    }
+}
