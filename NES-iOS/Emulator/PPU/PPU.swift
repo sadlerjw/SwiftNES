c6021f14c29588a0d00f051f12c848f2b0ce0ad1
Jason Sadler <jason@sadlerjw.com>
2025-08-05 21:06:27 -0400

    Do correct rendering (but we're not loading any data to render yet)

--- NES-iOS/Emulator/PPU/PPU.swift
+++ NES-iOS/Emulator/PPU/PPU.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 class PPU {
+    enum Layer {
+        case background
+        case sprite
+    }
+    
     unowned let bus : Bus
     
     // Registers exposed on the main bus:
@@ -30,6 +35,8 @@ class PPU {
     private var actualRegisters : Registers!
     var addressableRegisters : Registers { actualRegisters }
     
+    private(set) var renderingShiftRegisters = RenderingShiftRegisters()
+    
     // Internals:
     var oam = OAMTable()
     
@@ -47,11 +54,6 @@ class PPU {
     var scanline : Int = 261
     var cycle : Int = 0
     
-    // TODO: remove
-    var startR = Byte.random(in: 0...255)
-    var startG = Byte.random(in: 0...255)
-    var startB = Byte.random(in: 0...255)
-    
     init(bus: Bus) {
         self.bus = bus
         self.actualRegisters = Registers(ppu: self)
@@ -61,21 +63,53 @@ class PPU {
         previousFrame = Data(currentFrameBuffer)
     }
     
+    /// Returns an RGBA array
+    private func getColorFromPalette(layer: Layer, palette: Byte, value: Byte) -> [Byte] {
+        guard palette < 4 else { fatalError("Invalid palette index: \(palette)") }
+        guard value < 4 else { fatalError("Invalid palette value: \(value)") }
+        
+        let nesColor : Byte
+        
+        if value == 0 {
+            nesColor = bus.read(0x3F00)
+        } else {
+            
+            let paletteBaseAddress : Address
+            switch layer {
+            case .background:
+                paletteBaseAddress = 0x3F00
+            case .sprite:
+                paletteBaseAddress = 0x3F10
+            }
+            
+            nesColor = bus.read(paletteBaseAddress + Address(palette) * 4 + Address(value))
+        }
+        
+        let rgba = Color.colors[Int(nesColor)]
+        
+        // TODO: color tint bits: https://www.nesdev.org/wiki/NTSC_video#Color_Tint_Bits
+        
+        return rgba
+    }
+    
     func tick() {
         if (0 ..< 240).contains(scanline) {
             // Then this is a visible scanline
+
             if (1 ... 256).contains(cycle) {
-                let r = startR &+ Byte((scanline + cycle) % 256)
-                let g = startG &+ Byte((scanline + cycle) % 256)
-                let b = startB &+ Byte((scanline + cycle) % 256)
-                let a = UInt8(0xFF)
+                let pattern = renderingShiftRegisters.pattern(fineX: x)
+                let paletteIndex = renderingShiftRegisters.palette(fineX: x)
+                
+                renderingShiftRegisters.shift()
+                
+                let rgba = getColorFromPalette(layer: .background, palette: paletteIndex, value: pattern)
                 
                 let pixel = cycle - 1
                 
-                currentFrameBuffer[scanline * 256 * bytesPerPixel + pixel * bytesPerPixel + 0] =  r
-                currentFrameBuffer[scanline * 256 * bytesPerPixel + pixel * bytesPerPixel + 1] =  g
-                currentFrameBuffer[scanline * 256 * bytesPerPixel + pixel * bytesPerPixel + 2] =  b
-                currentFrameBuffer[scanline * 256 * bytesPerPixel + pixel * bytesPerPixel + 3] =  a
+                currentFrameBuffer[scanline * 256 * bytesPerPixel + pixel * bytesPerPixel + 0] =  rgba[0]
+                currentFrameBuffer[scanline * 256 * bytesPerPixel + pixel * bytesPerPixel + 1] =  rgba[1]
+                currentFrameBuffer[scanline * 256 * bytesPerPixel + pixel * bytesPerPixel + 2] =  rgba[2]
+                currentFrameBuffer[scanline * 256 * bytesPerPixel + pixel * bytesPerPixel + 3] =  rgba[3]
             }
         } else if scanline == 241 && cycle == 1 {
             status.insert(.vblank)
---
+++ NES-iOS/Emulator/PPU/RenderingShiftRegisters.swift
@@ -0,0 +1,53 @@
+//
+//  RenderingShiftRegisters.swift
+//  NES-iOS
+//
+//  Created by Jason Sadler on 2025-08-05.
+//
+
+extension PPU {
+    struct RenderingShiftRegisters {
+        private(set) var patternHigh : UInt16 = 0
+        private(set) var patternLow : UInt16 = 0
+        
+        private(set) var attributesHigh : UInt16 = 0
+        private(set) var attributesLow : UInt16 = 0
+        
+        mutating func loadPattern(high: Byte, low: Byte) {
+            patternHigh = patternHigh & 0x00FF | UInt16(high) << 8
+            patternLow = patternLow & 0x00FF | UInt16(low) << 8
+        }
+        
+        mutating func loadAttributes(high: Bool, low: Bool) {
+            attributesHigh = attributesHigh & 0x00FF | (high ? 0xFF00 : 0)
+            attributesLow = attributesLow & 0x00FF | (low ? 0xFF00 : 0)
+        }
+        
+        func pattern(fineX: UInt8) -> UInt8 {
+            return select(fineX: fineX, fromHigh: patternHigh, low: patternLow)
+        }
+        
+        func palette(fineX: UInt8) -> UInt8 {
+            return select(fineX: fineX, fromHigh: attributesHigh, low: attributesLow)
+        }
+        
+        private func select(fineX: UInt8, fromHigh high: UInt16, low: UInt16) -> Byte {
+            guard fineX < 8 else {
+                fatalError("FineX should never be greater than 7.")
+            }
+            let selector : UInt16 = 1 << fineX
+            
+            let highResult : UInt8 = (high & selector) > 0 ? 1 : 0
+            let lowResult : UInt8 = (low & selector) > 0 ? 1 : 0
+            
+            return highResult << 1 | lowResult
+        }
+        
+        mutating func shift() {
+            patternHigh = (patternHigh >> 1) | 0x8000
+            patternLow = (patternLow >> 1) | 0x8000
+            attributesHigh = (attributesHigh >> 1) | 0x8000
+            attributesLow = (attributesLow >> 1) | 0x8000
+        }
+    }
+}
